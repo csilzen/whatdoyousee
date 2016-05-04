c0a6f04821d1ccd87d35c1cf8616907e50299148
csilzen <silvacasandra@gmail.com>
2016-05-03 19:17:52 -0700

    rename app folder

---
+++ whatisthat/server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var router = require('express').Router();
+var port = 8080;
+
+app.use(express.static(__dirname + '/client')); // serves up index.html
+
+
+console.log('listening on port', port);
+app.listen(port);
+
+module.exports = app;
